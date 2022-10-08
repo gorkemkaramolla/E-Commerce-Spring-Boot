74edfc703f221ee4bd91752753d47fae63ad1335
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-10-09 01:37:53 +0300

    JWT base setup

---
+++ src/main/java/com/example/ecommerce/security/JwtTokenUtil.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtil {
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60* 60;
+    @Value("${jwt.secret}")
+    private String secret;
+
+
+
+    private String generateToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+}
