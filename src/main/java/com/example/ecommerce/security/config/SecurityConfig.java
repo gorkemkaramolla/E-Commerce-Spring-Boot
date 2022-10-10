2c31026024cc3980b5b77a7492f550e2493d4384
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-10-10 23:11:50 +0300

    Attempting authentication

--- src/main/java/com/example/ecommerce/security/config/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.ecommerce.security.config;
 
+import com.example.ecommerce.security.filter.AuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,8 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests().anyRequest().permitAll();
-        http.addFilter(null);
-
+        http.addFilter(new AuthenticationFilter(authenticationManagerBean()));
     }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
 
 }
--- src/main/java/com/example/ecommerce/security/filter/AuthenticationFilter.java
+++ src/main/java/com/example/ecommerce/security/filter/AuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.example.ecommerce.security.filter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -9,11 +12,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+@Slf4j
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationFilter(AuthenticationManager authenticationManager)
+    {
+        this.authenticationManager = authenticationManager;
+    }
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        log.info("Username is {}",username);
+        log.info("Password is {}",password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,password);
+        return authenticationManager.authenticate(authenticationToken);
+
     }
 
     @Override
