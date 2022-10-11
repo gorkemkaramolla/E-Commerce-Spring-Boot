30efc0ae1dac6355d4ee652445ba21e7c29afbdd
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-10-11 10:59:15 +0300

    Createing Tokens

--- src/main/java/com/example/ecommerce/security/filter/AuthenticationFilter.java
+++ src/main/java/com/example/ecommerce/security/filter/AuthenticationFilter.java
@@ -1,10 +1,15 @@
 package com.example.ecommerce.security.filter;
 
+import com.example.ecommerce.security.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +17,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+
 @Slf4j
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -33,6 +41,24 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        UserDetailsImpl user = (UserDetailsImpl) authResult.getPrincipal();
+        String access_token = Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 10 * 60 *1000))
+                .setIssuer(request.getRequestURL().toString())
+                .claim("roles",user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .signWith(SignatureAlgorithm.HS256,"gorkemkaramolla")
+                .compact();
+
+        String refresh_token = Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 30 * 60 *1000))
+                .setIssuer(request.getRequestURL().toString())
+                    .signWith(SignatureAlgorithm.HS256,"gorkemkaramolla")
+                .compact();
+        response.setHeader("access_token",access_token);
+        response.setHeader("refresh_token",refresh_token);
     }
 }
